ec7a5797c8cd88be06479e3b57c6c53acfc1d165
Dakota <bazookaharwell@gmail.com>
2017-03-15 16:21:46 -1000

    Test 17

--- arrayMethods.js
+++ arrayMethods.js
@@ -46,3 +46,4 @@ upToTen.splice(2, 0, 3, 4, 5, 6, 7, 8);
 brownOnly.splice(1, 6, "brown", "brown", "brown", "brown");
 
 orderedValues.unshift(1, 2, 3);
+var orderedValuesNewLength = orderedValues.length;
