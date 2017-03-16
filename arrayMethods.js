330e8e30926b3b6b3c0f6b7a820a07f4b771cd55
Dakota <bazookaharwell@gmail.com>
2017-03-15 16:24:52 -1000

    Tests 20 and 21

--- arrayMethods.js
+++ arrayMethods.js
@@ -51,3 +51,6 @@ var orderedValuesNewLength = orderedValues.length;
 var randomThingsArray = genericNumberArray.concat(colors);
 
 updatedOrders = orderQueue.concat([{ takeOut: 'Sushi' }, { takeOut: 'Pizza' }]);
+
+var sentence = scrambledWords.join(" ");
+var myFruits = fruitCollection.join (" + ");
